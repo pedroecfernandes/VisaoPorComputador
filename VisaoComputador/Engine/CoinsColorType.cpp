ec4d87390da6cb22d5421392d0b103fd28892778
Márcio Oliveira <moliveira.apps@gmail.com>
2018-06-02 16:19:03 +0100

    no message

--- VisaoComputador/Engine/CoinsColorType.cpp
+++ VisaoComputador/Engine/CoinsColorType.cpp
@@ -22,6 +22,7 @@ enum CoinType GetColorCoinTypeFromHSV(IplImage *hsvImage, IplImage *binaryImage,
 	int bytesperlineHSV = hsvImage->width*hsvImage->nChannels;
 	long int posBinary, posHSV;
 	double h, s, v;
+	double minH = 255, maxH = 0, minS = 255, maxS = 0, minV = 255, maxV = 0;
 	int countPixels = 0, i = 0, j = 0;
 	int colors[4];
 	int bytesperlineBinary = binaryImage->width*binaryImage->nChannels;
@@ -49,13 +50,11 @@ enum CoinType GetColorCoinTypeFromHSV(IplImage *hsvImage, IplImage *binaryImage,
 				s = ((double)hsvImage->imageData[posHSV+1] * 100.0) / 255.0;
 				v = ((double)hsvImage->imageData[posHSV+2] * 100.0) / 255.0;
 
-				printf("h: %f  | s: %f | v: %f\n", h, s, v);
-
-				if((h >= 20 && h <= 40) && (s >= 35 && s <= 75) && (v >= 20 && v <= 40))
+				if(h >= 20 && h <= 45)
 				{
 					colors[Bronze]++;
 				}
-				else if ((h >= 40 && h <= 60) && (s >= 30 && s <= 65) && (v >= 35 && v <= 50))
+				else if ((h >= 45 && h <= 70))
 				{
 					colors[Gold]++;
 				}
@@ -68,22 +67,39 @@ enum CoinType GetColorCoinTypeFromHSV(IplImage *hsvImage, IplImage *binaryImage,
 					colors[Undefined]++;
 				}
 
+				if (h > maxH)
+					maxH = h;
+				if (h < minH)
+					minH = h;
+				if (s > maxS)
+					maxS = s;
+				if (s < minS)
+					minS = s;
+				if (v > maxV)
+					maxV = v;
+				if (v < minV)
+					minV = v;
+
 				countPixels++;
 			}
 		}
 	}
 
+	/*printf("maxH: %f  | minH: %f\n", maxH, minH);
+	printf("maxS: %f  | minS: %f\n", maxS, minS);
+	printf("maxV: %f  | minV: %f\n\n", maxV, minV);*/
+
 	if (countPixels != 0)
 	{
-		if ((double)(colors[Bronze] / countPixels) > 0.85)
+		if (((double)colors[Bronze] / (double)countPixels) > 0.85)
 		{
 			return CoinType::DarkCoin;
 		}
-		else if ((double)(colors[Gold] / countPixels) > 0.85)
+		else if (((double)colors[Gold] / (double)countPixels) > 0.85)
 		{
 			return CoinType::GoldCoin;
 		}
-		else if (((double)(colors[Gold] / countPixels) > 0.65 && (double)(colors[Silver] / countPixels) > 0.10) || ((double)(colors[Silver] / countPixels) > 0.65 && (double)(colors[Gold] / countPixels) > 0.10))
+		else if ((((double)colors[Gold] / (double)countPixels) > 0.35 && ((double)colors[Silver] / (double)countPixels) > 0.20) || (((double)colors[Silver] / (double)countPixels) > 0.35 && ((double)colors[Gold] / (double)countPixels) > 0.20))
 		{
 			return CoinType::MixedCoin;
 		}
